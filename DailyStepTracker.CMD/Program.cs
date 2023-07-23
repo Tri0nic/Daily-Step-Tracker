c8a42edc65564a7bb93779eeecd5e55a1b65f4d7
Tri0nic <androyd0007@yandex.ru>
2023-07-23 23:12:50 +0300

    Добавлен контроллер пользователя и начальный интерфейс консоли

--- DailyStepTracker.CMD/Program.cs
+++ DailyStepTracker.CMD/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DailyStepTracker.BL.Controller;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите пол:");
+        string gender = Console.ReadLine();
+        Console.WriteLine("Введите дату рождения:");
+        DateTime birthday = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine("Введите вес:");
+        int weight = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите рост:");
+        int height = int.Parse(Console.ReadLine());
+        UserController userController = new UserController(name, gender, birthday, weight, height);
+        userController.Save();
+    }
+}
